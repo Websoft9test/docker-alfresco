a3eb37698646bd5c574ec638d3530b95da3abb22
qiaofeng1227 <76487013@qq.com>
2022-07-30 15:15:41 +0800

    Update encrypt.sh

--- src/encrypt.sh
+++ src/encrypt.sh
@@ -2,12 +2,9 @@
 export PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin
 clear
 
-# Add encrypt script below if use APP_ENCRYPT_PASSWORD at .env file
-
-# ------------- start -------------------------
-pip3 install passlib
-echo $1 > /tmp/password
+pip3 install passlib >/dev/null 2>&1
+echo "$1" > /tmp/password
 cd /data/apps/alfresco/src
-python3 encryption.py
+python3 encryption.py > /dev/null
 cat /tmp/password
-# ------------- end ---------------------------
+
